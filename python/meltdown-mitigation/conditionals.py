d177f1aa192a53d8aeca64e677ef60067c7f532e
Dawid <januch1993@gmail.com>
2022-05-02 19:49:12 +0200

    pass

--- python/meltdown-mitigation/conditionals.py
+++ python/meltdown-mitigation/conditionals.py
@@ -14,7 +14,10 @@ def is_criticality_balanced(temperature, neutrons_emitted):
     - The product of temperature and neutrons emitted per second is less than 500000.
     """
 
-    pass
+    if temperature < 800 and neutrons_emitted > 500 and temperature * neutrons_emitted < 500000:
+        return True
+    else: 
+        return False
 
 
 def reactor_efficiency(voltage, current, theoretical_max_power):
@@ -37,7 +40,18 @@ def reactor_efficiency(voltage, current, theoretical_max_power):
     where generated power = voltage * current
     """
 
-    pass
+    generated_power = voltage * current
+    percentage = (generated_power / theoretical_max_power) * 100 
+    if percentage >= 80:
+        return 'green'
+    elif percentage >= 60:
+        return 'orange'
+    elif percentage >= 30:
+        return 'red'
+    else:
+        return 'black'
+
+
 
 
 def fail_safe(temperature, neutrons_produced_per_second, threshold):
@@ -53,4 +67,11 @@ def fail_safe(temperature, neutrons_produced_per_second, threshold):
     3. 'DANGER' -> `temperature * neutrons per second` is not in the above-stated ranges
     """
 
-    pass
+    prod = temperature * neutrons_produced_per_second 
+    percentage = (prod / threshold) * 100
+    if percentage < 90:
+        return 'LOW'
+    elif 110 > percentage >= 90:
+        return 'NORMAL'
+    else:
+        return 'DANGER'
